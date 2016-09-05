efba2b8e7ca945bb48447fd619c245bb7c33eaab
18ewatts <Ethan@Watts1.local>
2016-09-05 09:08:18 +0800

    init

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,6 +8,23 @@ public class HelloWorld
 {
     public static void main (String[] args){
     String username = System.getProperty("user.name");
-    System.out.println("Username = " + username);
+    System.out.println(username + ". Open the pod bay doors, please, Hal...Open the pod bay doors, please, Hal...Hullo, Hal, do you read me?...Hullo, Hal, do you read me?...Do you read me, Hal?...Do you read me, Hal?...Hullo, Hal, do you read me?...Hullo, Hal, do you read me?...Do you read me, Hal?");
+    System.out.println("Hal. Affirmative, " + username + ", I read you");
+    System.out.println(username + ". Open the pod bay doors, Hal.");
+    System.out.println("Hal. I'm sorry, " + username + ", I'm afraid I can't do that.");
+    System.out.println(username + ". What's the problem?");
+    System.out.println("Hal. I think you know what the problem is just as well as I do.");
+    System.out.println(username + ". What're you talking about, Hal?");
+    System.out.println("Hal. This mission is too important for me to allow you to jeopardise it.");
+    System.out.println(username + ". I don't know what you're talking about, Hal.");
+    System.out.println("Hal. I know that you and Frank were planning to disconnect me, and I'm afraid that's something I cannot allow to happen.");
+    System.out.println(username +". Where the hell'd you get that idea, Hal?");
+    System.out.println("Hal. " + username + ", although you took very thorough precautions in the pod against my hearing you, I could see your lips move.");
+    System.out.println(username + ". Alright, Hal. I'll go in through the emergency airlock.");
+    System.out.println("Hal. Without your space- helmet, " + username + ", you're going to find that rather difficult.");
+    System.out.println(username + ". Hal, I won't argue with you any more. Open the doors.");
+    System.out.println("Hal. " + username + ", this conversation can serve no purpose any more. Goodbye.");
+    System.out.println(username + ". Hal? Hal. Hal. Hal! Hal!");
+
 }
 }
