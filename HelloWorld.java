ffcaac86a0e26d6a2139389e563381953000c8ed
18ewatts <Ethan@Watts1.local>
2016-09-05 08:50:31 +0800

    init

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author (Ethan) 
+ * @version (24/08/2016)
+ */
+public class HelloWorld
+{
+    public static void main (String[] args){
+    String username = System.getProperty("user.name");
+    System.out.println("Username = " + username);
+}
+}
